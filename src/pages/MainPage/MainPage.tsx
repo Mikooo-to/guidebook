a9135ba5c3d5e2b79386ee2fc4dd9c9af936f6dc
unknown <eugenes0809@gmail.com>
2024-07-19 19:10:12 +0300

    margins add

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Box, Button, TextField } from '@mui/material';
+import { Box, Button, TextField, Radio, RadioGroup, FormControlLabel, FormControl, FormLabel, } from '@mui/material';
 
 export function MainPage() {
   let [a, setA] = useState(0);
@@ -20,22 +20,40 @@ export function MainPage() {
   }
 
   return (
+    <Box>
+
     <Box
       width="25%"
       display={'flex'}
       flexDirection={'column'}
       alignContent={'start'}
-    >
-      <Box>{a}</Box>
-        <Button variant="contained" onClick={btnHandlerA} sx={{ color: 'red' }}>
+      marginTop={"20px"}
+      marginBottom={"20px"}
+      >
+      <Box style={{ marginBottom: '10px' }}>{a}</Box>
+        <Button style={{ marginBottom: '10px' }} variant="contained" onClick={btnHandlerA} sx={{ color: 'red' }}>
           +++
         </Button>
-        <Button variant="contained" onClick={btnHandlerS} sx={{ color: 'red' }}>
+        <Button style={{ marginBottom: '20px' }} variant="contained" onClick={btnHandlerS} sx={{ color: 'red' }}>
           ---
         </Button>
         <TextField onChange={(e) => textFieldChangeHandler(e.target.value)}>
           asdfas
         </TextField>
     </Box>
+
+    <FormControl>
+      <FormLabel id="demo-radio-buttons-group-label">Gender</FormLabel>
+      <RadioGroup
+      aria-labelledby="demo-radio-buttons-group-label"
+      defaultValue="female"
+      name="radio-buttons-group"
+      >
+        <FormControlLabel value="female" control={<Radio />} label="Female" />
+        <FormControlLabel value="male" control={<Radio />} label="Male" />
+        <FormControlLabel value="other" control={<Radio />} label="Other" />
+      </RadioGroup>
+    </FormControl>
+        </Box>
   );
 }

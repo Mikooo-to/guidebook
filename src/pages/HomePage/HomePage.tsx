c65e2ceefe119da7b6242eae893e1cbf356bc4f2
Ihor S <siafin2010@gmail.com>
2024-08-04 17:53:46 +0200

    get rid of css in separate files

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,5 +1,6 @@
 import { Box } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
+import backgroundImage from '../../images/background.jpg';
 export function HomePage() {
   const redir = useNavigate();
   function P1ButtonHandler() {
@@ -15,8 +16,36 @@ export function HomePage() {
     redir('/P4');
   }
   return (
-    <Box className="bg-image">
-      <button onClick={P1ButtonHandler}>центри гуманітарної допомоги</button>
+    <Box
+      sx={{
+        backgroundImage: `url(${backgroundImage})`,
+        backgroundSize: 'contain',
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: 'center',
+        height: '100vh',
+        width: '100vw',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        backgroundColor: 'black',
+        justifyContent: 'space-between',
+        alignSelf: 'flex-start',
+      }}
+    >
+      <button
+        style={{
+          backgroundColor: 'rgba(0, 0, 0, 0.7)',
+          padding: '15px 30px',
+          paddingLeft: '70px',
+          fontSize: '20px',
+          color: 'white',
+          cursor: 'pointer',
+          alignSelf: 'flex-start',
+        }}
+        onClick={P1ButtonHandler}
+      >
+        центри гуманітарної допомоги
+      </button>
       <button onClick={P2ButtonHandler}>торгівельні центри та магазини</button>
       <button onClick={P3ButtonHandler}>пам’ятки та цікавинки</button>
       <button onClick={P4ButtonHandler}>відпочинок</button>

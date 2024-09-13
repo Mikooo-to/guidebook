63e3b9155f154095f7e0191d56dc4a796cdfcda6
Ihor S <siafin2010@gmail.com>
2024-09-13 20:24:54 +0200

    use aurora serverless cluster

--- infra/cdk/lib/cdk-stack.ts
+++ infra/cdk/lib/cdk-stack.ts
@@ -4,6 +4,9 @@ import * as iam from 'aws-cdk-lib/aws-iam';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
 import * as apigateway from 'aws-cdk-lib/aws-apigateway';
+import * as rds from 'aws-cdk-lib/aws-rds';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as secretsmanager from 'aws-cdk-lib/aws-secretsmanager';
 import { Construct } from 'constructs';
 
 const projectName = 'guidebook';
@@ -161,5 +164,95 @@ export class MyStack extends cdk.Stack {
       value: lambdaFnMain.functionArn,
     });
     new cdk.CfnOutput(this, 'table', { value: table.tableName });
+
+    // =========================================
+    // =========================================
+    // Use the default VPC
+    const vpc = ec2.Vpc.fromLookup(this, 'VPC', {
+      isDefault: true,
+    });
+
+    // // Create a new public subnet
+    // const publicSubnet = new ec2.PublicSubnet(this, 'MyPublicSubnet', {
+    //   vpcId: vpc.vpcId,
+    //   availabilityZone: `${this.region}a`, // e.g., 'eu-central-1a'
+    //   cidrBlock: '172.31.48.0/20', // Make sure this doesn't overlap with existing subnets
+    // });
+
+    // // Create an Internet Gateway if it doesn't exist
+    // const igw = new ec2.CfnInternetGateway(this, 'MyInternetGateway');
+    // new ec2.CfnVPCGatewayAttachment(this, 'MyVPCGatewayAttachment', {
+    //   vpcId: vpc.vpcId,
+    //   internetGatewayId: igw.ref,
+    // });
+
+    // // Create a route table for the public subnet
+    // const publicRouteTable = new ec2.CfnRouteTable(this, 'MyPublicRouteTable', {
+    //   vpcId: vpc.vpcId,
+    // });
+
+    // // Associate the route table with the public subnet
+    // new ec2.CfnSubnetRouteTableAssociation(
+    //   this,
+    //   'MySubnetRouteTableAssociation',
+    //   {
+    //     routeTableId: publicRouteTable.ref,
+    //     subnetId: publicSubnet.subnetId,
+    //   },
+    // );
+
+    // // Add a route to the Internet Gateway
+    // new ec2.CfnRoute(this, 'MyPublicRoute', {
+    //   routeTableId: publicRouteTable.ref,
+    //   destinationCidrBlock: '0.0.0.0/0',
+    //   gatewayId: igw.ref,
+    // });
+
+    // Create a security group for the database
+    const dbSecurityGroup = new ec2.SecurityGroup(this, 'DbSecurityGroup', {
+      vpc,
+      allowAllOutbound: true,
+      description: 'Security group for Aurora database',
+    });
+
+    // allow inbound traffic from anywhere to the db
+    dbSecurityGroup.addIngressRule(
+      ec2.Peer.anyIpv4(),
+      ec2.Port.tcp(5432), // allow inbound traffic on port 5432 (postgres)
+      'allow inbound traffic from anywhere to the db on port 5432',
+    );
+    // ------------------
+    //   ---------------
+    // aws rds describe-db-engine-versions | jq '.DBEngineVersions[] | select(.SupportedEngineModes != null and .SupportedEngineModes[] == "serverless" and .Engine == "aurora-postgresql")'
+    // -------------
+    // -------------
+
+    const dbCluster = new rds.ServerlessCluster(this, 'Database', {
+      engine: rds.DatabaseClusterEngine.auroraPostgres({
+        version: rds.AuroraPostgresEngineVersion.VER_13_12,
+      }),
+      vpc,
+      vpcSubnets: {
+        subnetType: ec2.SubnetType.PUBLIC,
+      },
+      securityGroups: [dbSecurityGroup],
+      scaling: {
+        autoPause: cdk.Duration.minutes(5),
+        minCapacity: rds.AuroraCapacityUnit.ACU_2,
+        maxCapacity: rds.AuroraCapacityUnit.ACU_2,
+      },
+      defaultDatabaseName: 'guidebook',
+      credentials: rds.Credentials.fromGeneratedSecret('postgres'), // This will create the secret automatically
+    });
+
+    new cdk.CfnOutput(this, 'DBEndpoint', {
+      value: dbCluster.clusterEndpoint.hostname,
+      description: 'The endpoint of the database',
+    });
+
+    new cdk.CfnOutput(this, 'DBCredentialsSecret', {
+      value: dbCluster.secret?.secretName ?? 'Secret not found',
+      description: 'The secret name for database credentials',
+    });
   }
 }

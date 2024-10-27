a40416e9b15be725993d8d8a839c5bf9123315ff
Ihor S <siafin2010@gmail.com>
2024-10-27 15:30:35 +0100

    Revert "some stuff" trial to add debug to docker
    This reverts commit e7163e1349a6b7e7183a63b88dc4d647bdfafcc7.

--- docker-local/api/docker-entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ "$DEBUG_MODE" = "true" ]; then
-    # Start Lambda function with debugger
-    exec node --inspect=0.0.0.0:9229 /usr/local/bin/aws-lambda-rie /var/runtime/bootstrap
-else
-    # Start Lambda function normally
-    exec /var/runtime/bootstrap
-fi

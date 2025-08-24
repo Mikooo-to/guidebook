4c4f4cdfb382fcf3c8b75f143a01ec221b63ea78
Ihor S <siafin2010@gmail.com>
2025-08-24 17:10:55 +0200

    forbid new admins creation

--- api/lambdas/src/lambda-api/modules/users/users.service.ts
+++ api/lambdas/src/lambda-api/modules/users/users.service.ts
@@ -4,25 +4,24 @@ import { TListResponce } from '../articles/articles.service';
 import { BaseService } from '../baseService';
 import { CreateUserDto } from './dto/createUser.dto';
 import * as bcrypt from 'bcrypt';
+import { UserRole } from './constants';
 
 export class UsersService extends BaseService {
   async create(userDto: CreateUserDto): Promise<User> {
+    if (userDto.role === UserRole.ADMIN) {
+      throw new Error('Creation of new admins is not allowed');
+    }
     userDto.password = bcrypt.hashSync(userDto.password, 8);
     const user = Object.assign(new User(), userDto);
-    const result = await this.dbConnection.entityManager.create<User>(
-      user,
-    );
+    const result = await this.dbConnection.entityManager.create<User>(user);
     return result;
   }
 
   async findAll(): Promise<TListResponce<User>> {
-    const res = await this.dbConnection.entityManager.find<User>(
-      User,
-      {},
-    );
+    const res = await this.dbConnection.entityManager.find<User>(User, {});
     return res;
   }
-  
+
   async findByEmail(email: string): Promise<User | undefined> {
     const user = await this.dbConnection.entityManager.findOne<User>(User, {
       email,

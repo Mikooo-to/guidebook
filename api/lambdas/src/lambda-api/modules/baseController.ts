92e610596c40eacf462648ac9adaa1f22d0e84d1
Ihor S <siafin2010@gmail.com>
2025-08-24 20:32:08 +0200

    add auth check  to POST sections

--- api/lambdas/src/lambda-api/modules/baseController.ts
+++ api/lambdas/src/lambda-api/modules/baseController.ts
@@ -1,6 +1,29 @@
 import { Connection } from '@typedorm/core';
-import { API } from 'lambda-api';
+import { API, NextFunction, Request, Response } from 'lambda-api';
+import jwt from 'jsonwebtoken';
+import { TUserTokenData } from '../types/types';
+const JWT_SECRET = process.env.JWT_SECRET;
 
 export class BaseController {
   constructor(protected api: API, protected dbConnection: Connection) {}
+
+  checkTokenAuth(req: Request, res: Response): TUserTokenData {
+    const token = req.headers.authorization?.split(' ')[1];
+    if (!token) {
+      res.status(401).send({ message: 'Unauthorized' });
+      throw new Error('Unauthorized');
+    }
+    if (!JWT_SECRET) {
+      throw new Error('No JWT_SECRET found. Check env variables');
+    }
+
+    try {
+      const decodedToken = jwt.verify(token, JWT_SECRET);
+      console.log(decodedToken);
+      return decodedToken as TUserTokenData;
+    } catch (error) {
+      res.status(401).send({ message: 'Unauthorized' });
+      throw new Error('Unauthorized');
+    }
+  }
 }
